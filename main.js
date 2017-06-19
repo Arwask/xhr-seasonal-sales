614e84f3815e3424171900d57b5d06ac32d3e4bd
Arwa <arwa.it15@gmail.com>
2017-06-19 14:44:15 -0500

    create xhr to fetch categories

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
-let reqProduct = new XMLHttpRequest();
-let reqCategories = new XMLHttpRequest();
 
+
+//get products
 function displayProducts()
 {
 	let productArray = JSON.parse(event.target.responseText).products;
 	console.log(productArray);
 }
 
+function displayCategories()
+{
+	let catArray = JSON.parse(event.target.responseText).categories;
+	console.log(catArray);
+}
 
-reqProduct.addEventListener("load", displayProducts)
+function getCategories()
+{
+	let reqCategories = new XMLHttpRequest();
+	reqCategories.addEventListener("load", displayCategories)
+	reqCategories.open("GET","data/categories.json");
+	reqCategories.send();	
+}
 
-reqProduct.open("GET","data/products.json");
-reqProduct.send();
-reqCategories.open("GET","data/categories.json");
-reqCategories.send();
+function getProducts()
+{
+	let reqProduct = new XMLHttpRequest();
+	reqProduct.addEventListener("load",displayProducts )
+	reqProduct.open("GET","data/products.json");
+	reqProduct.send();
+}
+
+getProducts();
+getCategories();
+//get categories
