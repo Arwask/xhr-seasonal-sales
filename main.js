0889895e7b589b82351bf1d6d35929da1522735c
Arwa <arwa.it15@gmail.com>
2017-06-19 13:45:14 -0500

    create xhr to fetch products

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("hi")
+let reqProduct = new XMLHttpRequest();
+let reqCategories = new XMLHttpRequest();
+
+function displayProducts()
+{
+	let productArray = JSON.parse(event.target.responseText).products;
+	console.log(productArray);
+}
+
+
+reqProduct.addEventListener("load", displayProducts)
+
+reqProduct.open("GET","data/products.json");
+reqProduct.send();
+reqCategories.open("GET","data/categories.json");
+reqCategories.send();
