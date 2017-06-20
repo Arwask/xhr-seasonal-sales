538af2b48eb891f3ef94d1df8f3816af5403de33
Arwa <arwa.it15@gmail.com>
2017-06-20 07:40:17 -0500

    create function to get department names of products

--- main.js
+++ main.js
@@ -1,22 +1,48 @@
 
-
+let products = null;
+let categories = null;
 //get products
-function displayProducts()
+
+function buildDOMObj()
 {
-	let productArray = JSON.parse(event.target.responseText).products;
-	console.log(productArray);
+	//TODO: loop through products and categories to grab Prod name, Dept, Price and Category ID
+	let productArr = products.map( function(prod)
+	{
+		// inside this loop we need to loop again through categories to find the category whose id matched cat_id of current prod. may be a filter. Returned array will contain one object. we can set dept on new obj we are making with the "name " property.
+		let catItem = categories.filter( function(category)
+		{
+			return category.id === prod.category_id;
+		})
+		let prodObj = {dept : catItem[0].name};
+		return prodObj; 
+	})
+	console.log("product array", productArr);
+}
+function buildCard(prodObj)
+{
+	let card = `<div class="prodCard">
+					<h2>${prodObj.name}</h2>
+					<h3>${prodObj.dept}</h3>
+					<p>${prodObj.price}</p>
+				</div>`;
+	return card;
 }
 
-function displayCategories()
+function setProducts()
 {
-	let catArray = JSON.parse(event.target.responseText).categories;
-	console.log(catArray);
+	products = JSON.parse(event.target.responseText).products;
+	getCategories();
+}
+function setCategories()
+{
+	categories = JSON.parse(event.target.responseText).categories;
+	buildDOMObj();
 }
 
 function getCategories()
 {
 	let reqCategories = new XMLHttpRequest();
-	reqCategories.addEventListener("load", displayCategories)
+	reqCategories.addEventListener("load", setCategories)
 	reqCategories.open("GET","data/categories.json");
 	reqCategories.send();	
 }
@@ -24,11 +50,9 @@ function getCategories()
 function getProducts()
 {
 	let reqProduct = new XMLHttpRequest();
-	reqProduct.addEventListener("load",displayProducts )
+	reqProduct.addEventListener("load",setProducts )
 	reqProduct.open("GET","data/products.json");
 	reqProduct.send();
 }
 
 getProducts();
-getCategories();
-//get categories
